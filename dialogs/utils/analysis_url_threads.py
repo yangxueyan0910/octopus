351e1c7badebf953323a143d6acb4ef57d1720c5
yangxueyan0910 <1953603070@qq.com>
2025-01-19 16:30:28 +0800

    能下载视频选集

--- dialogs/utils/analysis_url_threads.py
+++ dialogs/utils/analysis_url_threads.py
@@ -57,9 +57,9 @@ class AnalysisUrlThread(QThread):
 
     def get_video_info(self, html_url):
         video_url_pattern1 = r'https://www\.bilibili\.com/video/[\w/]+'
-        pattern2 = r"https://search\.bilibili\.com/all\?keyword=.*"
-        pattern3 = r'https://search\.bilibili\.com/all\?.*?keyword=([^&]+)'
-        pattern4 = r'https://search\.bilibili\.com/video\?keyword=([^&]+)'
+        # pattern2 = r"https://search\.bilibili\.com/all\?keyword=.*"
+        # pattern3 = r'https://search\.bilibili\.com/all\?.*?keyword=([^&]+)'
+        # pattern4 = r'https://search\.bilibili\.com/video\?keyword=([^&]+)'
         if re.match(video_url_pattern1, html_url):
             response = self.get_response(html_url)
             '''print(response.text)'''
@@ -109,7 +109,6 @@ class AnalysisUrlThread(QThread):
                 return video_info_list, is_season_display, author_name, total
             else:
                 # 获取视频标题
-
                 bvid1 = json_data_1['videoData']['bvid']
                 total=len(json_data_1['videoData']['pages'])
                 print(total)
@@ -125,8 +124,8 @@ class AnalysisUrlThread(QThread):
                 else:
                     for i in range(0,total):
                       element=(json_data_1['videoData']['pages'][i]['part'])
-                      re.sub(r'[\/:*?"<>|\s]', '', element)
-                      html_url=('https://www.bilibili.com/video/'+str(bvid1)+'/?p='+str(i+1))
+                      re.sub(r'[\/:*?"<>|\s]', '', element)#将字符串element中的/、 :、*、?、 "、 <、 >、|和空白字符\s替换为空字符串''
+                      html_url=('https://www.bilibili.com/video/'+str(bvid1)+'/?p='+str(i+1))#如网址“https://www.bilibili.com/video/BV1V54y1y7c4/?p=2”
                       item_video_info=[element,html_url,bvid1]
                       video_info_list.append(item_video_info)
                       print(video_info_list[i])
@@ -136,57 +135,57 @@ class AnalysisUrlThread(QThread):
                 if total>1:
                   is_season_display= True
                 return video_info_list, is_season_display, author_name, total
-        elif re.match(pattern2, html_url)or re.match(pattern3, html_url) or re.match(pattern4,html_url):
-
-            #keyword_match = re.search(r'keyword=([^&]+)', html_url)
-            #keyword=keyword_match.group(1)
-            page_match = re.search(r'page=(\d+)', html_url)
-            if page_match:
-                page = int(page_match.group(1))
-            else:
-                page = 1
-            parsed_url = urllib.parse.urlparse(html_url)
-            query_params = urllib.parse.parse_qs(parsed_url.query)
-            if 'keyword' in query_params:
-                # 解码关键词
-                decoded_keyword = urllib.parse.unquote(query_params['keyword'][0])
-
-            else:
-                print("No 'keyword' parameter found in the URL.")
-            print(decoded_keyword,page)
-            info_url = "https://api.bilibili.com/x/web-interface/wbi/search/type?category_id=&search_type=video&ad_resource=5654&page={}&keyword={}".format(page,decoded_keyword)
-            print(info_url)
-            response = self.get_response1(info_url)
-            json_data = json.loads(response.text)
-            if 'code' in json_data:
-                if json_data['code'] == -403:
-                    self.process.emit("解析异常")
-                    self.quit()
-                    return
-            print(119, json_data['code'])
-            video_info_list = []
-            total = len(json_data['data']['result'])
-            author_name = json_data['data']['result'][0]['author']
-            for i, item in enumerate(json_data['data']['result']):
-                if self.stop:
-                    break
-                bvid = item['bvid']
-                item_title1 = item['title']
-                item_title1 = re.sub(r'[\/:*?"<>|\s]', '', item_title1)  # 去除双引号
-                # item_title = re.sub(r'[^\u4e00-\u9fa5]+', '', item_title)  # 去除双引号
-                item_title = re.sub(r'emclass=keyword.*?em', f'{decoded_keyword}', item_title1)
-                item_url = "https://www.bilibili.com/video/" + str(bvid)
-                item_video_info = [item_title, item_url, bvid]
-
-                video_info_list.append([])
-                video_info_list[i] = item_video_info
-                print(video_info_list[i])
-                time.sleep(0.2)
-                self.process.emit("正在解析数据源... {}%".format(round((i + 1) / total, 2) * 100))
-                # 设置保存个数
-                # if i == 1:
-                #     return video_info_list, is_season_display, author_name, counter
-            return video_info_list, True, author_name, total
+        # elif re.match(pattern2, html_url)or re.match(pattern3, html_url) or re.match(pattern4,html_url):
+        #
+        #     #keyword_match = re.search(r'keyword=([^&]+)', html_url)
+        #     #keyword=keyword_match.group(1)
+        #     page_match = re.search(r'page=(\d+)', html_url)
+        #     if page_match:
+        #         page = int(page_match.group(1))
+        #     else:
+        #         page = 1
+        #     parsed_url = urllib.parse.urlparse(html_url)
+        #     query_params = urllib.parse.parse_qs(parsed_url.query)
+        #     if 'keyword' in query_params:
+        #         # 解码关键词
+        #         decoded_keyword = urllib.parse.unquote(query_params['keyword'][0])
+        #
+        #     else:
+        #         print("No 'keyword' parameter found in the URL.")
+        #     print(decoded_keyword,page)
+        #     info_url = "https://api.bilibili.com/x/web-interface/wbi/search/type?category_id=&search_type=video&ad_resource=5654&page={}&keyword={}".format(page,decoded_keyword)
+        #     print(info_url)
+        #     response = self.get_response1(info_url)
+        #     json_data = json.loads(response.text)
+        #     if 'code' in json_data:
+        #         if json_data['code'] == -403:
+        #             self.process.emit("解析异常")
+        #             self.quit()
+        #             return
+        #     print(119, json_data['code'])
+        #     video_info_list = []
+        #     total = len(json_data['data']['result'])
+        #     author_name = json_data['data']['result'][0]['author']
+        #     for i, item in enumerate(json_data['data']['result']):
+        #         if self.stop:
+        #             break
+        #         bvid = item['bvid']
+        #         item_title1 = item['title']
+        #         item_title1 = re.sub(r'[\/:*?"<>|\s]', '', item_title1)  # 去除双引号
+        #         # item_title = re.sub(r'[^\u4e00-\u9fa5]+', '', item_title)  # 去除双引号
+        #         item_title = re.sub(r'emclass=keyword.*?em', f'{decoded_keyword}', item_title1)
+        #         item_url = "https://www.bilibili.com/video/" + str(bvid)
+        #         item_video_info = [item_title, item_url, bvid]
+        #
+        #         video_info_list.append([])
+        #         video_info_list[i] = item_video_info
+        #         print(video_info_list[i])
+        #         time.sleep(0.2)
+        #         self.process.emit("正在解析数据源... {}%".format(round((i + 1) / total, 2) * 100))
+        #         # 设置保存个数
+        #         # if i == 1:
+        #         #     return video_info_list, is_season_display, author_name, counter
+        #     return video_info_list, True, author_name, total
 
         else:
             flag1 = re.findall('com/(.*?)/', html_url)
@@ -216,7 +215,7 @@ class AnalysisUrlThread(QThread):
                         break
                     bvid = item['bvid']
                     item_title = item['title']
-                    item_title = re.sub(r'[\/:*?"<>|\s]', '', item_title)  # 去除双引号
+                    item_title = re.sub(r'[\/:*?"<>|\s]', '', item_title)
                     # item_title = re.sub(r'[^\u4e00-\u9fa5]+', '', item_title)  # 去除双引号
                     item_url = "https://www.bilibili.com/video/" + str(bvid)
                     item_video_info = [item_title, item_url, bvid]
@@ -269,8 +268,7 @@ class AnalysisUrlThread(QThread):
                         break
                     bvid = item['bvid']
                     item_title = item['title']
-                    item_title = re.sub(r'[\/:*?"<>|\s]', '', item_title)  # 去除双引号
-                    # item_title = re.sub(r'[^\u4e00-\u9fa5]+', '', item_title)  # 去除双引号
+                    item_title = re.sub(r'[\/:*?"<>|\s]', '', item_title) #将字符串 element 中的/、 :、*、?、 "、 <、 >、|和空白字符\s替换为空字符串''
                     item_url = "https://www.bilibili.com/video/" + str(bvid)
                     item_video_info = [item_title, item_url, bvid]
 
@@ -287,49 +285,50 @@ class AnalysisUrlThread(QThread):
 
     def get_response(self, html_url):
         headers = {
-            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 SLBrowser/9.0.0.10191 SLBChan/105",
-
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
+            'cookie':"buvid3=CD5F63EB-4942-00B5-09B3-B3A8367AF6EA40758infoc; b_nut=1730386940; _uuid=7148F10D10-128B-2FA1-3F5B-714A78B6F6A953155infoc; rpdid=|()k~m|RJ)Y0J'u~J|J)~JlR; buvid4=C97989C2-D5F5-E07A-BFCC-ED0898C377DD40758-024103115-uG3KEHERXsjv%2B7SHY9SX5g%3D%3D; header_theme_version=CLOSE; iflogin_when_web_push=1; enable_web_push=DISABLE; buvid_fp=f442b4341c4d6bfa57c13c877418398c; CURRENT_QUALITY=80; bp_t_offset_40648342=1016659883275059200; browser_resolution=1389-671; home_feed_column=4; DedeUserID=693862727; DedeUserID__ckMd5=97bc1d93000f822e; LIVE_BUVID=AUTO8617367642142762; PVID=16; bsource=search_bing; bmg_af_switch=1; bmg_src_def_domain=i1.hdslb.com; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3Mzc0NDY3MDAsImlhdCI6MTczNzE4NzQ0MCwicGx0IjotMX0.gsHh1YJLXzFtwqwbj95mAC0STfNawlaVvgOVjeyr2rY; bili_ticket_expires=1737446640; SESSDATA=dc4b0060%2C1752739501%2C06e4f%2A11CjCDcPOkh-Gv4O7cP1dWxqzc393OF8CTW2FpuViB7na7vMk6BenQdWpHNgwyPYUkCOkSVnZCQ21jUFRRMlVxcTRvNGtyU2hWb2Y4bnZWZjVqaWNwZ1luemdBYXcxd1NBSGpSbW80SUFYWXo0MUh4cGtRN0haV1N1RDk0STBkajVJQmNDWG9qLUJnIIEC; bili_jct=d084778ee7db30b9e946629a5b73216c; sid=77s4rpf9; bp_t_offset_693862727=1023716510247419904; CURRENT_FNVAL=4048; b_lsid=B96C7521_1947D0D4015",
             # Referer: 防盗链。用于告诉服务器我是从哪个链接跳转来的。
             'Referer': 'https://www.bilibili.com/',
 
         }
         # time.sleep(random.randint(1, 5))
         with requests.Session() as s:
-            response = s.get(url=html_url, headers=headers, timeout=5)
+            response = s.get(url=html_url, headers=headers, timeout=10)
         return response
-    def get_response1(self,html_url):
-        headers = {
-            'accept': 'application/json,text/plain,*/*',
-            'accept-encoding': 'gzip,deflate,br',
-            'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-            'cookie': "buvid4=9049BB59-1F85-A9BD-0DA2-9192AE167A5D36258-022072714-FtODpuifXpkq25XCF6uzpg%3D%3D; CURRENT_FNVAL=4048; CURRENT_QUALITY=16; rpdid=|(J~lkkYRuR~0J'uY~~|~uulk; _uuid=A751C7A5-9E1B-E9D10-EEDB-C969EE9FD35D32373infoc; b_nut=1694182632; buvid3=FBA1AF8F-4C58-6B14-167E-93872E3E9B3E33168infoc; header_theme_version=CLOSE; home_feed_column=4; browser_resolution=1280-647; DedeUserID=503413226; DedeUserID__ckMd5=35968ed52e86c9f4; LIVE_BUVID=AUTO1016952932183001; buvid_fp_plain=undefined; buvid_fp=590f6a10b94100f60ad0045a787f6933; enable_web_push=DISABLE; fingerprint=6cde34c5e5743a15dd719569af6fd0d3; PVID=3; bp_video_offset_503413226=859170776947884037; innersign=0; b_lsid=54F76EC6_18B93D20FF9; bsource=search_baidu; SESSDATA=13bde86e%2C1714544102%2C51c98%2Ab1CjAgTGB454yNo_tM7gcjVZJRkvBMCye2zKdSDkh6jTVvZ6UpDQve5sNsv2KMqs3qyIASVjh4Si1jc2ZJQV9EX2pHZGpQTFExb2ptclgya3AxZHY2UGdtWm8zNzhNWENMeEh4ZEZxWDBWZVpXQkVfZHBNb2pWaVEtb2M5MFVLZVdOendoaXJCMGVRIIEC; bili_jct=e02876481ffa068b23e02a1d5e0e5011; sid=5rpazcm3",
-            'referer': 'https://www.bilibili.com/',
-            'origin': 'https://search.bilibili.com',
-            'sec-ch-ua-mobile': '?0',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="8"',
-            # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 SLBrowser/8.0.1.4031 SLBChan/105'
-        }
-        params = {
-            'search_type': 'video',
-            '__refresh__': 'true',
-            'ad_resource': '5654',
-            '_extra': '',
-            'context': '',
-            'page_size': '42',
-            'from_source': '',
-            'from_spmid': '333.337',
-            'platform': 'pc',
-            'highlight': '1',
-            'single_column': '0',
-            'qv_id': 'Yx1wQ928UHWUTbvfP7E13tvczsX3ce80',
-            'category_id': '',
-            'preload': 'true',
-            'com2co': 'true',
-        }
-        with requests.Session() as s:
-            response = s.get(url=html_url, headers=headers,params=params, timeout=5)
-        return response
+
+    # def get_response1(self,html_url):
+    #     headers = {
+    #         'accept': 'application/json,text/plain,*/*',
+    #         'accept-encoding': 'gzip,deflate,br',
+    #         'accept-language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    #         'cookie':"buvid3=CD5F63EB-4942-00B5-09B3-B3A8367AF6EA40758infoc; b_nut=1730386940; _uuid=7148F10D10-128B-2FA1-3F5B-714A78B6F6A953155infoc; rpdid=|()k~m|RJ)Y0J'u~J|J)~JlR; buvid4=C97989C2-D5F5-E07A-BFCC-ED0898C377DD40758-024103115-uG3KEHERXsjv%2B7SHY9SX5g%3D%3D; header_theme_version=CLOSE; iflogin_when_web_push=1; enable_web_push=DISABLE; buvid_fp=f442b4341c4d6bfa57c13c877418398c; CURRENT_QUALITY=80; bp_t_offset_40648342=1016659883275059200; browser_resolution=1389-671; home_feed_column=4; DedeUserID=693862727; DedeUserID__ckMd5=97bc1d93000f822e; LIVE_BUVID=AUTO8617367642142762; PVID=16; bsource=search_bing; bmg_af_switch=1; bmg_src_def_domain=i1.hdslb.com; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3Mzc0NDY3MDAsImlhdCI6MTczNzE4NzQ0MCwicGx0IjotMX0.gsHh1YJLXzFtwqwbj95mAC0STfNawlaVvgOVjeyr2rY; bili_ticket_expires=1737446640; SESSDATA=dc4b0060%2C1752739501%2C06e4f%2A11CjCDcPOkh-Gv4O7cP1dWxqzc393OF8CTW2FpuViB7na7vMk6BenQdWpHNgwyPYUkCOkSVnZCQ21jUFRRMlVxcTRvNGtyU2hWb2Y4bnZWZjVqaWNwZ1luemdBYXcxd1NBSGpSbW80SUFYWXo0MUh4cGtRN0haV1N1RDk0STBkajVJQmNDWG9qLUJnIIEC; bili_jct=d084778ee7db30b9e946629a5b73216c; sid=77s4rpf9; bp_t_offset_693862727=1023716510247419904; CURRENT_FNVAL=4048; b_lsid=B96C7521_1947D0D4015",
+    #          'referer': 'https://www.bilibili.com/',
+    #         'origin': 'https://search.bilibili.com',
+    #         'sec-ch-ua-mobile': '?0',
+    #         'sec-fetch-dest': 'empty',
+    #         'sec-fetch-mode': 'cors',
+    #         'sec-fetch-site': 'same-site',
+    #         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="8"',
+    #         # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
+    #         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 SLBrowser/8.0.1.4031 SLBChan/105'
+    #     }
+    #     params = {
+    #         'search_type': 'video',
+    #         '__refresh__': 'true',
+    #         'ad_resource': '5654',
+    #         '_extra': '',
+    #         'context': '',
+    #         'page_size': '42',
+    #         'from_source': '',
+    #         'from_spmid': '333.337',
+    #         'platform': 'pc',
+    #         'highlight': '1',
+    #         'single_column': '0',
+    #         'qv_id': 'Yx1wQ928UHWUTbvfP7E13tvczsX3ce80',
+    #         'category_id': '',
+    #         'preload': 'true',
+    #         'com2co': 'true',
+    #     }
+    #     with requests.Session() as s:
+    #         response = s.get(url=html_url, headers=headers,params=params, timeout=5)
+    #     return response
